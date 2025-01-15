a19a1507c051939d02187f646be6e4d130072ac6
Mailton Junior <mailtonjr7@gmail.com>
2025-01-15 15:44:00 -0300

    Password Encode

--- password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$B2uPTgvH0wBJ5TUnB9LcNOEF1h5DKZAbD756XTgtb63LYBuMF38dq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$B2uPTgvH0wBJ5TUnB9LcNOEF1h5DKZAbD756XTgtb63LYBuMF38dq');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
